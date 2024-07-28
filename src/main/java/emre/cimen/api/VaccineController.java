be8b660a0ef651666bc16545f3c787d208271516
emrecimen20 <emrecimen48@outlook.com>
2024-07-28 16:23:07 +0300

    Add all vaccine operations.

--- src/main/java/emre/cimen/api/VaccineController.java
+++ src/main/java/emre/cimen/api/VaccineController.java
@@ -1,25 +1,87 @@
 package emre.cimen.api;
 
+import emre.cimen.core.config.modelMapper.IModelMapperService;
+import emre.cimen.core.result.Result;
 import emre.cimen.core.result.ResultData;
+import emre.cimen.core.utilies.ResultHelper;
+import emre.cimen.dto.CursorResponse;
 import emre.cimen.dto.request.vaccine.VaccineSaveRequest;
+import emre.cimen.dto.request.vaccine.VaccineUpdateRequest;
 import emre.cimen.dto.response.vaccine.VaccineResponse;
+import emre.cimen.entities.Vaccine;
 import emre.cimen.service.abstracts.IVaccineService;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/vaccines")
 public class VaccineController {
     private final IVaccineService vaccineService;
+    private final IModelMapperService modelMapperService;
 
-    public VaccineController(IVaccineService vaccineService) {
+    public VaccineController(IVaccineService vaccineService, IModelMapperService modelMapperService) {
         this.vaccineService = vaccineService;
+        this.modelMapperService = modelMapperService;
     }
 
+    // Registeration vaccine operation.
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public ResultData<VaccineResponse> save(@Valid @RequestBody VaccineSaveRequest vaccineSaveRequest){
         return this.vaccineService.save(vaccineSaveRequest);
     }
+
+    // Display vaccine by ID.
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<VaccineResponse> get(@PathVariable("id") int id){
+        Vaccine vaccine = this.vaccineService.get(id);
+        VaccineResponse vaccineResponse = this.modelMapperService.forResponse().map(vaccine,VaccineResponse.class);
+        return ResultHelper.success(vaccineResponse);
+    }
+    // Delete vaccine operation.
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable int id){
+        this.vaccineService.delete(id);
+        return ResultHelper.ok();
+    }
+
+    // Updating vaccine operation.
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<VaccineResponse> update(@Valid @RequestBody VaccineUpdateRequest vaccineUpdateRequest){
+        return this.vaccineService.update(vaccineUpdateRequest);
+    }
+
+    // Display vaccine get by animal ID.
+    @GetMapping("/animal/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<List<VaccineResponse>> getVaccineByAnimalId(@PathVariable("id") int animalId){
+        return this.vaccineService.findByAnimalId(animalId);
+    }
+
+    // Display vaccine by date.
+    @GetMapping("/findByDate")
+    public ResultData<List<VaccineResponse>> getVaccinesByDate(
+            @RequestParam(name = "entryDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate entryDate,
+            @RequestParam(name = "exitDate") @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate exitDate
+    ){
+        return this.vaccineService.findByDate(entryDate,exitDate);
+    }
+
+    // Display all vaccine operation.
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<VaccineResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        return this.vaccineService.cursor(page, pageSize);
+    }
+
 }
--- src/main/java/emre/cimen/service/concretes/VaccineManager.java
+++ src/main/java/emre/cimen/service/concretes/VaccineManager.java
@@ -1,7 +1,9 @@
 package emre.cimen.service.concretes;
 
+import emre.cimen.core.config.ConvertEntityToResponse;
 import emre.cimen.core.config.modelMapper.IModelMapperService;
 import emre.cimen.core.exception.DataAlreadyExistException;
+import emre.cimen.core.exception.NotFoundException;
 import emre.cimen.core.result.ResultData;
 import emre.cimen.core.utilies.Msg;
 import emre.cimen.core.utilies.ResultHelper;
@@ -14,6 +16,9 @@ import emre.cimen.entities.Vaccine;
 import emre.cimen.repositories.VaccineRepo;
 import emre.cimen.service.abstracts.IAnimalService;
 import emre.cimen.service.abstracts.IVaccineService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -24,21 +29,25 @@ public class VaccineManager implements IVaccineService {
     private final VaccineRepo vaccineRepo;
     private final IModelMapperService modelMapperService;
     private final IAnimalService animalService;
+    private final ConvertEntityToResponse<Vaccine, VaccineResponse> convert;
 
-    public VaccineManager(VaccineRepo vaccineRepo, IModelMapperService modelMapperService, IAnimalService animalService) {
+    public VaccineManager(VaccineRepo vaccineRepo, IModelMapperService modelMapperService, IAnimalService animalService, ConvertEntityToResponse<Vaccine, VaccineResponse> convert) {
         this.vaccineRepo = vaccineRepo;
         this.modelMapperService = modelMapperService;
         this.animalService = animalService;
+        this.convert = convert;
     }
 
     @Override
     public Vaccine get(int id) {
-        return null;
+        return this.vaccineRepo.findById(id).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        Vaccine vaccine = this.get(id);
+        this.vaccineRepo.delete(vaccine);
+        return true;
     }
 
     @Override
@@ -51,7 +60,7 @@ public class VaccineManager implements IVaccineService {
         Vaccine saveVaccine = this.modelMapperService.forRequest().map(vaccineSaveRequest, Vaccine.class);
         saveVaccine.setAnimal(animal);
         if (!existVaccines.isEmpty() && existVaccines.get(0).getProtectionFnshDate().isAfter(LocalDate.now())){
-            return ResultHelper.error("Aynı koda sahip aşının bitiş tarihi bitmemiş! ");
+            return ResultHelper.error("The vaccine with the same code has not expired ! ");
         }
         if (!existVaccines.isEmpty()){
             throw new DataAlreadyExistException(Msg.getEntityForMsg(Vaccine.class));
@@ -61,26 +70,38 @@ public class VaccineManager implements IVaccineService {
 
     @Override
     public ResultData<VaccineResponse> update(VaccineUpdateRequest vaccineUpdateRequest) {
-        return null;
+        this.get(vaccineUpdateRequest.getId());
+        Vaccine updateVaccine = this.modelMapperService.forRequest().map(vaccineUpdateRequest, Vaccine.class);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(updateVaccine, VaccineResponse.class));
     }
 
     @Override
     public ResultData<CursorResponse<VaccineResponse>> cursor(int page, int pageSize) {
-        return null;
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Vaccine> vaccinePage = this.vaccineRepo.findAll(pageable);
+        Page<VaccineResponse> vaccineResponsePage = vaccinePage.map(vaccine -> this.modelMapperService.forResponse().map(vaccine, VaccineResponse.class));
+        return ResultHelper.cursor(vaccineResponsePage);
     }
 
     @Override
     public ResultData<List<VaccineResponse>> findByAnimalId(int id) {
-        return null;
+        List<Vaccine> vaccineList = this.vaccineRepo.findByAnimalId(id);
+        List<VaccineResponse> vaccineResponseList = this.convert.convertToResponseList(vaccineList, VaccineResponse.class);
+        return ResultHelper.success(vaccineResponseList);
     }
 
     @Override
     public ResultData<List<VaccineResponse>> findByDate(LocalDate entryDate, LocalDate exitDate) {
-        return null;
+        List<Vaccine> vaccineList = this.vaccineRepo.findByprotectionFnshDateBetween(entryDate, exitDate);
+        List<VaccineResponse> vaccineResponseList = this.convert.convertToResponseList(vaccineList, VaccineResponse.class);
+        return ResultHelper.success(vaccineResponseList);
     }
 
     @Override
     public List<Vaccine> findByCodeAndName(String code, String name) {
         return List.of();
+    }
+    public boolean isTrue(){
+        return true;
     }
 }
